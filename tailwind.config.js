2c11a16561cdcf988c5eb1c9bc8bc13a21a3c52e
ShivamBole <shivamofficial285@gmail.com>
2025-12-30 17:51:57 +0530

    it's an version two frontend

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,44 +1,6 @@
-<<<<<<< HEAD
-// module.exports = {
-//   content: ['./src/**/*.{js,jsx,ts,tsx}'],
-//   theme: {
-//     fontFamily: {
-//       primary: 'Orbitron',
-//       secondary: 'Rajdhani',
-//       tertiary: 'Aldrich',
-//     },
-//     container: {
-//       padding: {
-//         DEFAULT: '15px',
-//       },
-//     },
-//     screens: {
-//       sm: '640px',
-//       md: '768px',
-//       lg: '960px',
-//       xl: '1200px',
-//     },
-//     extend: {
-//       colors: {
-//         primary: '#0a0a0a',
-//         accent: '#B809C3',
-//       },
-//       backgroundImage: {
-//         site: "url('./assets/site-bg.jpg')",
-//         about: "url('./assets/about.png')",
-//         services: "url('./assets/services.png')",
-//       },
-//     },
-//   },
-//   plugins: [],
-// };
 
 
 
-
-
-=======
->>>>>>> 6d77287952ed76ed6cb77ad660cf97657c74eb44
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
@@ -62,7 +24,6 @@ module.exports = {
       colors: {
         primary: '#0a0a0a',
         accent: '#B809C3',
-<<<<<<< HEAD
         marsRed: '#ff2f92',
         marsPurple: '#7b2cff',
         marsBlue: '#3bace2',
@@ -89,14 +50,6 @@ module.exports = {
         services: "url('./assets/services.png')",
       },
 
-=======
-      },
-      backgroundImage: {
-        site: "url('./assets/site-bg.jpg')",
-        about: "url('./assets/about.png')",
-        services: "url('./assets/services.png')",
-      },
->>>>>>> 6d77287952ed76ed6cb77ad660cf97657c74eb44
     },
   },
   plugins: [],
